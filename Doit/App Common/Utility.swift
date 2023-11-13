b50b43b2cc69c23928f4290275a7ebc7591aacbd
Sanjeeb201 <sanjeeb.appknit@gmail.com>
2023-11-13 08:56:48 +0530

    Repeated Task Completion Issues Fixed.
    On click of check button, repetitive task get removed.

--- Doit/App Common/Utility.swift
+++ Doit/App Common/Utility.swift
@@ -230,3 +230,8 @@ func getPriorityString(priority : Priority) -> String {
 }
 
 
+//MARK:- Delay Features
+func delay(_ delay:Double, closure:@escaping ()->()) {
+    DispatchQueue.main.asyncAfter(
+        deadline: DispatchTime.now() + Double(Int64(delay * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC), execute: closure)
+}
